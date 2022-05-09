40eb2c4e14be059a45844f72513e8aca87c675d6
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-05-09 14:50:56 +0300

    added the user inteface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,21 @@
+import models.Cipher;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App{
-  public static void main (String[] args){
-      System.out.println("hello world");
+  public static void main (String[] args) throws IOException {
+      System.out.println("Welcome to Caesars Cipher");
+      System.out.println("Please enter word or sentence for Encryption.");
+      BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+      String inputString = input.readLine();
+      System.out.println("Please enter the cipher digit 1-10.");
+      BufferedReader inputNumberString = new BufferedReader(new InputStreamReader(System.in));
+      Integer inputInteger = Integer.parseInt(inputNumberString.readLine());
+      String output = new Cipher().startEncryption(inputString,inputInteger);
+      System.out.println(output );
+
   }
 
 }
